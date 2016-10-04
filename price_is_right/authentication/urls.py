6740bbfa79f7e84f14351dc1f627cb397323ae35
Leon Song <ls3233@columbia.edu>
2016-10-04 15:41:50 -0400

    WORKING AUTH

--- price_is_right/authentication/urls.py
+++ price_is_right/authentication/urls.py
@@ -5,5 +5,6 @@ import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^/register/$', views.register, name='register'),
+    url(r'^/login/$', views.user_login, name='login'),
 ]
 
--- price_is_right/authentication/views.py
+++ price_is_right/authentication/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from forms import UserForm, UserProfileForm
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.contrib.auth import authenticate, login
 
 
 def index(request):
-    return HttpResponse("DOUBLE CHECKING")
+    return HttpResponse("SUCCESSFULLY LOGGED IN")
 
 
 def register(request):
@@ -54,16 +54,14 @@ def register(request):
         profile_form = UserProfileForm()
 
     # Render the template depending on the context.
-    return render_to_response(
-        'register.html',  # Django knows to look for register.html within authentication/templates
-        {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
-        context)
+
+    return render(request, "register.html", {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
 def user_login(request):
     context = RequestContext(request)
 
-    # If the request is a HTTP POST, try to pull out the relevant information.
+    # If the request is a HTTP POST, try to pull out the relevant information
     if request.method == 'POST':
         # Gather the username and password provided by the user.
         # This information is obtained from the login form.
@@ -83,7 +81,7 @@ def user_login(request):
                 # If the account is valid and active, we can log the user in.
                 # We'll send the user back to the homepage.
                 login(request, user)
-                return HttpResponseRedirect('/authentication/')  # note to Aaron: index is here for now
+                return HttpResponseRedirect('/authentication')  # note to Aaron: index is here for now
             else:
                 # An inactive account was used - no logging in!
                 return HttpResponse("Your Rango account is disabled.")
@@ -97,5 +95,5 @@ def user_login(request):
     else:
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
-        return render_to_response('login.html', {}, context)
+        return render(request, "login.html", {})
 
